0def23cc0de6f2af56ee131092da9f982bfd2bee
nlg-96 <76234601+nlg-96@users.noreply.github.com>
2025-04-01 11:53:56 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,8 +2,30 @@
 document.getElementById("toggleSidebarBtn").addEventListener("click", function() {
   const sidebar = document.querySelector(".sidebar");
   const content = document.querySelector(".content");
-  
+
   // Toggling class 'hidden' trên sidebar và content
   sidebar.classList.toggle("hidden");
   content.classList.toggle("hidden");
+});
+
+// Chức năng toggle các mục con khi click vào mục chính
+const menuItems = document.querySelectorAll('.toggle-menu > a');
+
+menuItems.forEach(item => {
+  item.addEventListener('click', function(event) {
+    const submenu = this.nextElementSibling; // lấy menu con tương ứng
+
+    // Đóng tất cả các menu con khác
+    const allSubmenus = document.querySelectorAll('.submenu');
+    allSubmenus.forEach(submenuItem => {
+      if (submenuItem !== submenu) {
+        submenuItem.style.display = 'none'; // Ẩn các menu con khác
+      }
+    });
+
+    // Toggle hiển thị menu con của mục vừa bấm
+    if (submenu) {
+      submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+    }
+  });
 });
